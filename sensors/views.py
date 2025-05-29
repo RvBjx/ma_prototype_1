98ccb0c67df422d94f22c963bfee3bb9987e1a18
Roman Bühlmann <rvbjx07@gmail.com>
2025-05-29 21:17:07 +0200

    Delete sensors directory

--- sensors/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Sensor, SensorReading
-
-def dashboard(request):
-    sensors = Sensor.objects.all()
-    readings = SensorReading.objects.order_by('-timestamp')[:50]
-    return render(request, 'sensors/dashboard.html', {'sensors': sensors, 'readings': readings})
